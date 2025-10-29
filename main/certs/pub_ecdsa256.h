3b0d1780e3a6ec348ca1d92e470d5b1fde972d0d
0d3ng <noprianto@polnema.ac.id>
2025-10-29 09:41:08 +0900

    update key public pem

--- main/certs/pub_ecdsa256.h
+++ main/certs/pub_ecdsa256.h
@@ -1,6 +1,6 @@
 // ECDSA P-256 public key in PEM format
 static const unsigned char PUBLIC_KEY_PEM_P256[] =
     "-----BEGIN PUBLIC KEY-----\n"
-    "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAESzTtTEqBhAbcGCRV1YkAE3OF9TLL\n"
-    "t83zTsjujwLtvGs82WqUeEycthet1aG/Y0bhi8s6sEoXrd+RG/C2JbjCpQ==\n"
+    "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEZ6wyNNruJ9hi9HWpUaEsp9QjgTST\n"
+    "wBvoYjePyNgQvCO8P6+YjLRgWnFCc3cT11KUuhzmgvCmIIo/O8HFcnqTaw==\n"
     "-----END PUBLIC KEY-----\n";
